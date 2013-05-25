8359f4341b04da682f21dfe5ad621dbe971764c1
shivgadhia <shivam.gadhia@googlemail.com>
2013-05-25 09:45:48 +0100

    Updated version. Pruned database before doing an insert. This fixes the scenario where you want to add the same secret code you just deleted.

--- src/com/shivgadhia/android/SecretCodeLauncher/persistance/SecretCodesWriter.java
+++ src/com/shivgadhia/android/SecretCodeLauncher/persistance/SecretCodesWriter.java
@@ -13,14 +13,14 @@ public class SecretCodesWriter {
     }
 
     public void saveSecretCode(String activityName, String secretCode) {
-
+        prune();
         ContentValues values = new ContentValues();
         values.put(Tables.SecretCodes.COL_ACTIVITY_NAME, activityName);
         values.put(Tables.SecretCodes.COL_SECRET_CODE, secretCode);
         databaseWriter.saveDataToTable(Tables.TBL_SECRET_CODES, values);
     }
 
-    public void setToDelete(SecretCode secretCode){
+    public void setToDelete(SecretCode secretCode) {
 
         ContentValues values = new ContentValues();
         values.put(Tables.SecretCodes.COL_ACTIVITY_NAME, secretCode.getActivityName());
@@ -31,14 +31,14 @@ public class SecretCodesWriter {
     }
 
     public void prune() {
-        databaseWriter.delete(Tables.TBL_SECRET_CODES,Tables.SecretCodes.COL_HIDDEN+" =?", new String[]{"1"});
+        databaseWriter.delete(Tables.TBL_SECRET_CODES, Tables.SecretCodes.COL_HIDDEN + " =?", new String[]{"1"});
     }
 
     public void unDelete(ArrayList<Integer> deletedIds) {
-        for(Integer id : deletedIds){
+        for (Integer id : deletedIds) {
             ContentValues values = new ContentValues();
             values.put(Tables.SecretCodes.COL_HIDDEN, 0);
-            databaseWriter.update(Tables.TBL_SECRET_CODES, values, Tables.SecretCodes.COL_ID+"=?", new String[]{id.toString()});
+            databaseWriter.update(Tables.TBL_SECRET_CODES, values, Tables.SecretCodes.COL_ID + "=?", new String[]{id.toString()});
 
         }
     }
